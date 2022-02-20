2909adfa3cdcc49a4b2baa2fdd16c3c7c4c52fb4
zhou yang <zhouyang123200@hotmail.com>
2022-02-20 16:30:15 +0800

    feat: complete field text difference show

--- text_diff/__manifest__.py
+++ text_diff/__manifest__.py
@@ -24,6 +24,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'views/templates.xml',
         'views/views.xml',
     ],
 }
--- text_diff/models/models.py
+++ text_diff/models/models.py
@@ -1,12 +1,28 @@
-# -*- coding: utf-8 -*-
-
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 
 class LawTerm(models.Model):
     _name = 'text_diff.law_term'
     _description = '法律条款'
+    _rec_name = "name"
 
     name = fields.Char()
+    content = fields.Char('content')
     description = fields.Html()
+    parent_id = fields.Many2one('text_diff.law_term', string='parent', copy=False)
+    child_ids = fields.One2many('text_diff.law_term', 'parent_id', string='child')
 
+    @api.constrains('child_ids')
+    def _constrains_child_ids(self):
+        for record in self:
+            if len(record.child_ids) > 1:
+                raise UserError('two childs!')
+
+    def generate_other_version(self):
+        self.ensure_one()
+        new_record = self.copy()
+        new_record.parent_id = self
+        action_id = self.env.ref('text_diff.law_term_act')
+        action_id['res_id'] = new_record.id
+        return action_id
